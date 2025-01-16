8769d1ca38bee3f780dda3bb049df4b5ac15b735
SALMAN LAREB KHAN <salmanaxl7643@gmail.com>
2025-01-16 20:44:13 +0530

    Add files via upload

---
+++ codeX-ID/birthday.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python2
+#coding=utf-8
+#KABIR SINGH
+#programer KABIR SINGH
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00.01)
+
+#### colours ####
+B='\033[1;94m'
+R='\033[1;91m'
+G='\033[1;92m'
+W='\033[1;97m'
+S='\033[1;96m'
+P='\033[1;95m'
+Y='\033[1;93m'
+os.system("clear")
+jalan('\033[1;95m------------------------------------------------')
+jalan('\033[1;94m   _     _     _     _     _     _     _     _  ')
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+jalan('\033[1;94m------------------------------------------------')
+jalan('\033[1;91m HAPPY BIRTHDAY༒SWATI SINGH༒ ====>')
+jalan('\033[1;94m HAPPY BIRTHDAY ༒SWATI SINGH༒=====>')
+jalan('\033[1;92m HAPPY BIRTHDAY ༒SWATI SINGH༒======>')
+jalan('\033[1;95m HAPPY BIRTHDAY ༒SWATI SINGH༒=======>')
+jalan('\033[1;91m HAPPY BIRTHDAY ༒SWATI SINGH༒========>')
+jalan('\033[1;94m HAPPY BIRTHDAY ༒SWATI SINGH༒=========>')
+jalan('\033[1;92m HAPPY BIRTHDAY ༒SWATI SINGH༒==========>')
+jalan('\033[1;95m HAPPY BIRTHDAY ༒SWATI SINGH༒===========>')
+jalan('\033[1;91m HAPPY BIRTHDAY ༒SWATI SINGH༒============>')
+jalan('\033[1;94m HAPPY BIRTHDAY ༒SWATI SINGH༒=============>')
+jalan('\033[1;92m HAPPY BIRTHDAY ༒SWATI SINGH༒==============>')
+jalan('\033[1;95m HAPPY BIRTHDAY ༒SWATI SINGH༒===============>')
+jalan('\033[1;91m HAPPY BIRTHDAY ༒SWATI SINGH༒================>')
+jalan('\033[1;94m HAPPY BIRTHDAY ༒SWATI SINGH༒=================>')
+jalan('\033[1;92m HAPPY BIRTHDAY ༒SWATI SINGH༒==================>')
+jalan('\033[1;95m HAPPY BIRTHDAY ༒SWATI SINGH༒===================>')
+jalan('\033[1;91m HAPPY BIRTHDAY ༒SWATI SINGH༒====================>')
+jalan('\033[1;94m HAPPY BIRTHDAY ༒SWATI SINGH༒=====================>')
+jalan('\033[1;92m HAPPY BIRTHDAY ༒SWATI SINGH༒======================>')
+jalan('\033[1;95m HAPPY BIRTHDAY ༒SWATI SINGH༒======================>')
+jalan('\033[1;93m------------------------------------------------')
+jalan('\033[1;94m   _     _     _     _     _     _     _     _  ')
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+jalan('\033[1;91m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+jalan('\033[1;94m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+jalan('\033[1;92m------------------------------------------------')
+###KABIR SINGH###
+## PROGRAMMER ##
+os.system("clear")
+print """\033[1;90m"""
+jalan('\033[1;93m------------------------------------------------')
+jalan('\033[1;93m------------------------------------------------')
+jalan('\033[1;91m _    _              _____    _____   __     __')
+jalan('\033[1;91m| |  | |     /\     |  __ \  |  __ \  \ \   / /')
+jalan('\033[1;94m| |__| |    /  \    | |__) | | |__) |  \ \_/ / ')
+jalan('\033[1;91m|  __  |   / /\ \   |  ___/  |  ___/    \   /  ')
+jalan('\033[1;94m| |  | |  / ____ \  | |      | |         | |   ')
+jalan('\033[1;91m|_|  |_| /_/    \_\ |_|      |_|         |_|   ')
+jalan('\033[1;95m------------------------------------------------')
+jalan('\033[1;94m   _     _     _     _     _     _     _     _  ')
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+jalan('\033[1;91m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+jalan('\033[1;94m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+jalan('\033[1;94m------------------------------------------------')
+jalan('\033[1;95m  HAPPY BIRTHDAY  SWATI  ')
+jalan('\033[1;94m   GOD BLESS YOU  ')
+jalan('\033[1;91m  ALWAYS BE HAPPY IN YOUR LIFE ')
+jalan('\033[1;91m------------------------------------------------')
+jalan('\033[1;94m   [->  ROGRAMMER KABIR SINGH <-]')
+jalan('\033[1;91m------------------------------------------------')
+jalan('\033[1;92m------------------------------------------------')
+jalan('\033[1;94m>BIRTHDAY WISH BY KABIR SINGH <')
+jalan('\033[1;92m------------------------------------------------')
+jalan('\033[1;91m      >  CODED BY KABIR SINGH <')
+jalan('\033[1;94m------------------------------------------------')
+birthday= "KABIR SINGH"
